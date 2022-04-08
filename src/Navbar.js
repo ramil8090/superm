6c98ed501dd79421dc532cec4710260a24de2053
Ramil <ramil8090@gmail.com>
2022-04-08 23:42:48 +0500

    Dark theme

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,31 @@
-import React, {useContext} from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { AppContext } from "./AppContext";
+import Button from "./Button";
 
 export default function Navbar(props) {
   const app = useContext(AppContext);
   const cartCount = app.getCartCount();
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  useEffect(() => {
+    const preferDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    if (preferDark) {
+      setIsDarkTheme(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isDarkTheme) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }, [isDarkTheme]);
+
+  function handleThemeClick() {
+    setIsDarkTheme(!isDarkTheme);
+  }
 
   return (
     <nav className="navbar">
@@ -12,6 +33,9 @@ export default function Navbar(props) {
         SuperM
       </NavLink>
       <ul>
+        <li className="nav-item">
+          <Button className="theme-switcher" onClick={handleThemeClick}>{isDarkTheme ? "Dark" : "Light"}</Button>
+        </li>
         <li className="nav-item">
           <NavLink exact activeClassName="active" to="/">
             Home
